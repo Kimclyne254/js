65d082bc626311c729ca00280099e033c0f0c1fe
lennox chebii <lennoxchebii2@gmail.com>
2023-11-09 11:08:22 +0300

    first commit

---
+++ new.js
@@ -0,0 +1,11 @@
+let count = 30
+
+while (count > 0) {
+    console.log("current count is" + count);
+    count = count - 1;
+}
+
+
+//DOM - document object model code
+document.querySelector("h1").textContent = "A new heading"
+document.querySelector("h1").style.color = "purple"
